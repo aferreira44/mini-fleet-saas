251a846675ddf978aab97e9196c375f399b34433
André Ferreira <hello@andreferreira.me>
2025-05-29 11:12:38 +0100

    Add CORS middleware to backend application
    - Integrated CORSMiddleware to handle cross-origin requests.
    - Configured middleware to allow all origins, credentials, methods, and headers.

--- backend/mini_fleet_saas/main.py
+++ backend/mini_fleet_saas/main.py
@@ -1,5 +1,16 @@
 from connexion import AsyncApp
+from connexion.middleware import MiddlewarePosition
+from starlette.middleware.cors import CORSMiddleware
 
 app = AsyncApp(__name__)
+
+app.add_middleware(
+    CORSMiddleware,
+    position=MiddlewarePosition.BEFORE_EXCEPTION,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.add_api("../openapi.yaml")
