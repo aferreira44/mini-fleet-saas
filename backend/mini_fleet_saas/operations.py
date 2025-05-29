ad2f8eff487dafe1d6e5af2cd971775df211b7cb
André Ferreira <hello@andreferreira.me>
2025-05-29 11:12:07 +0100

    Refactor vehicle data management with vehicles.json
    - Introduced vehicles.json to store vehicle information and statuses.
    - Updated operations.py to read from and write to vehicles.json instead of data.json for vehicle management functions.

--- backend/mini_fleet_saas/operations.py
+++ backend/mini_fleet_saas/operations.py
@@ -2,14 +2,14 @@ import json
 
 
 def get_vehicles():
-    with open("data.json", "r") as f:
+    with open("./data/vehicles.json", "r") as f:
         return json.load(f)["vehicles"]
 
 
 def update_vehicle_status(vehicle_id: int, body: dict):
-    with open("data.json", "r") as f:
+    with open("./data/vehicles.json", "r") as f:
         data = json.load(f)
     data["vehicles"][vehicle_id - 1]["status"] = body["status"]
-    with open("data.json", "w") as f:
+    with open("./data/vehicles.json", "w") as f:
         json.dump(data, f)
     return data["vehicles"][vehicle_id - 1]
